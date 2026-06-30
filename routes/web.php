261ff3363e532d04b7dd34bf7a307a341d4bde23
EkyRizkyRamadhan <ekyramadhan1232@gmail.com>
2026-06-30 11:04:40 +0700

    fix: add missing /dashboard route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 // Bungkus route dengan middleware auth agar wajib login
 Route::middleware('auth')->group(function () {
     // Route Generator Resep
